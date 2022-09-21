f0740dd6ea256795f5176211a59ab496172dce09
christopher lopez <christopherwlopez89@gmail.com>
2022-09-21 09:14:42 -0700

    creating components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+// Your App component should render a Hender, a Footer and a Main component, each of which is defined in their own files. 
+
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Main from './Main';
+import Footer from '/Footer';
 
 class App extends React.component {
   render() {
     return (
-      // 
       <div className="App">
-        <h1>Horned Beasts</h1>
+        <Header/>
+        <Main/>
+        <Footer/>
       </div>
     );
   }
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+  render() {
+     return (
+        <h3>item here</h3>
+    )       
+  }
+}            
+            
+export default Main;
