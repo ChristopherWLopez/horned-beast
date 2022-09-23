d5fe9c7f45779fe7079680723f08266fa24e7521
christopher lopez <christopherwlopez89@gmail.com>
2022-09-22 17:47:46 -0700

    added image display feature where i pulled from my data.json file and passed them through my props in order to render the image

--- src/Main.js
+++ src/Main.js
@@ -9,12 +9,12 @@ class Main extends React.Component {
         <HornedBeast 
         title={data[0].title}
         description={data[0].description}
-        imageURL={data[0].imageURL}
+        imageURL={data[0].imgURL}
         />
         <HornedBeast
-        title={"Unicorn Head"}
-        description={"Someone wearing a very silly unicorn head mask"}
-        imageURL={"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"}
+        title={data[1].title}
+        description={data[1].description}
+        imageURL={data[1].imgURL}
         />
         </>
     )       
